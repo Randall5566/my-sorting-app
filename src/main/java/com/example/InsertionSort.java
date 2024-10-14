4c24b7a2abf538faf85c707f95365f377b0730ba
vi-techsys <paultnd@gmail.com>
2024-10-14 15:54:53 +0100

    sort function

--- src/main/java/com/example/InsertionSort.java
+++ src/main/java/com/example/InsertionSort.java
@@ -1,5 +1,33 @@
 package com.example;
 
 public class InsertionSort {
-    
+
+    /* This function sorts an array using insertion sort */
+    public static void insertionSort(int[] arr) {
+        int n = arr.length;
+        for (int i = 1; i < n; ++i) {
+            int key = arr[i];
+            int j = i - 1;
+
+            while (j >= 0 && arr[j] > key) {
+                arr[j + 1] = arr[j];
+                j = j - 1;
+            }
+            arr[j + 1] = key;
+            System.out.println(arr[0]);
+            System.out.println(arr[1]);
+            System.out.println(arr[2]);
+            System.out.println(arr[3]);
+        }
+    }
+
+
+    public static void main(String[] args) {
+        int[] arr = { 12, 11, 13, 5, 6 };
+
+        // Call insertion sort
+        insertionSort(arr);
+
+        
+    }
 }
