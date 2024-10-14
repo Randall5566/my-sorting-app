d0e60fb73b2e8f9c187b3d0140292d7b194fa5d2
vi-techsys <paultnd@gmail.com>
2024-10-14 16:20:45 +0100

    insertion sort function

--- src/main/java/com/example/InsertionSort.java
+++ src/main/java/com/example/InsertionSort.java
@@ -6,11 +6,11 @@ import org.json.simple.JSONObject;
 public class InsertionSort {
     private static int count = 0;
 
-  // Get array of cupcake names
+  // get array of cupcake names
     public static String[] nameArray(JSONArray cupcakeArray) {
         String[] arr = new String[cupcakeArray.size()];
 
-        // Get names from JSON objects
+        // get names from JSON objects
         for (int i = 0; i < cupcakeArray.size(); i++) {
             JSONObject o = (JSONObject) cupcakeArray.get(i);
             String name = (String) o.get("name");
@@ -22,16 +22,49 @@ public class InsertionSort {
     public static void main(String[] args) {
         String fileName = "cupcake_3906.json";
         
-        // Read cupcake names
+        // read cupcake names
         JSONArray cupcakeArray = JSONFile.readArray(fileName);
         String[] cupcakeNameArray = nameArray(cupcakeArray);
         
-        // Print unsorted list
+        // print unsorted list
         System.out.println("----- Unsorted array -----");
         print(cupcakeNameArray);
+
+          // call insertion sort function
+          insertionSort(cupcakeNameArray);
+
+         // print sorted list
+         System.out.println("----- Sorted array -----");
+         print(cupcakeNameArray);
+ 
+         // print statistics
+         System.out.println("----- Statistics -----");
+         System.out.printf("Size of array = %d\n", cupcakeNameArray.length);
+         System.out.printf("Count = %d\n", count);
     }
     
-     // Print cupcake array
+     // insertion sort function
+     public static void insertionSort(String[] arr) {
+        int n = arr.length;
+        for (int i = 1; i < n; ++i) {
+            String key = arr[i];
+            int j = i - 1;
+
+            // We move elements of arr[0..i-1], that are greater than key,
+            // to one position ahead of their current position
+            while (j >= 0 && arr[j].compareTo(key) > 0) {
+                arr[j + 1] = arr[j];
+                j = j - 1;
+                // Increase count for each comparison
+                count++;
+            }
+            arr[j + 1] = key;
+            // Increase count for placing the key
+            count++;
+        }
+    }
+
+     // print the array
      public static void print(String[] cupcakeNameArray) {
         System.out.printf("Number\tName\n");
         System.out.printf("------\t---------------\n");
